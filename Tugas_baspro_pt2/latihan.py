d31006f1ae7920b516a3d3fafd5bb60c198ffff9
Najwa Andara Putri <najwaputri1979@gmail.com>
2025-03-19 23:38:01 +0700

    first commit

---
+++ Tugas_baspro_pt2/latihan.py
@@ -0,0 +1,14 @@
+umur = int(input("Masukkan umur: "))
+
+if umur <= 5:
+    print("balita")
+elif umur > 5 and umur <= 13:
+    print("anak-anak")
+elif umur > 13 and umur <= 25:
+    print("remaja")
+elif umur > 25 and umur <= 35:
+    print("dewasa")
+elif umur > 35 and umur <= 55:
+    print("orang tua")
+else:
+    print("lansia")
